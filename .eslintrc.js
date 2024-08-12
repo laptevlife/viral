c0962cc038d4d3abd4d35de49f7ea6ccee1374ad
Лаптев Михаил Евгеньевич <melaptev@phoenixit.ru>
2024-08-13 02:55:14 +0400

    test build

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // other configurations
+  rules: {
+    // other rules
+    'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
+    '@typescript-eslint/no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }]
+  }
+};
